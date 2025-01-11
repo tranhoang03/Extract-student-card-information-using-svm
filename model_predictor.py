82a8d6ac283b4745febb9e385455611f8e08bd4a
tranhoang05 <vhoahusc@gmail.com>
2025-01-11 14:48:08 +0700

    Update model_predictor.py

--- model_predictor.py
+++ model_predictor.py
@@ -1,20 +1,23 @@
 import cv2
 import numpy as np
-import pickle
+from joblib import load
 
 class ModelPredictor:
     def __init__(self, model_paths):
         self.models = {label: self.load_model(path) for label, path in model_paths.items()}
 
     def load_model(self, path):
-        with open(path, 'rb') as file:
-            model = pickle.load(file)
+        try:
+            model = load(path)
+        except KeyError as e:
+            print(f"Lỗi khi tải mô hình: {e}")
+            model = None
         return model
 
     def predict_info(self, extracted_info):
         predictions = {}
         for label, cropped_image in extracted_info.items():
-            if label in self.models:
+            if label in self.models and self.models[label] is not None:
                 gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
                 resized = cv2.resize(gray, (128, 128))
                 img = resized / 255.0
@@ -24,5 +27,5 @@ class ModelPredictor:
                 predicted_class = model.predict(flattened)[0]
                 predictions[label] = predicted_class
             else:
-                predictions[label] = "Mô hình không tồn tại"
+                predictions[label] = "Mô hình không tồn tại hoặc không thể tải"
         return predictions
