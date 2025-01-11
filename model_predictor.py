7b6ffb3f044f244993ace58032d077dd5e82818a
tranhoang05 <vhoahusc@gmail.com>
2025-01-11 15:45:51 +0700

    Update model_predictor.py

--- model_predictor.py
+++ model_predictor.py
@@ -1,15 +1,31 @@
 import cv2
 import numpy as np
 from joblib import load
+import os
 
 class ModelPredictor:
     def __init__(self, model_paths):
-        self.models = {label: load(path) for label, path in model_paths.items()}
+        self.models = {label: self.load_model(path) for label, path in model_paths.items()}
+
+    def load_model(self, path):
+        if not os.path.exists(path):
+            print(f"Không tìm thấy file mô hình tại đường dẫn: {path}")
+            return None
+        try:
+            model = load(path)
+            print(f"Đã tải mô hình từ: {path}")
+        except KeyError as e:
+            print(f"Lỗi khi tải mô hình từ {path}: {e}")
+            model = None
+        except Exception as e:
+            print(f"Lỗi không xác định khi tải mô hình từ {path}: {e}")
+            model = None
+        return model
 
     def predict_info(self, extracted_info):
         predictions = {}
         for label, cropped_image in extracted_info.items():
-            if label in self.models:
+            if label in self.models and self.models[label]:
                 gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
                 resized = cv2.resize(gray, (128, 128))
                 img = resized / 255.0
@@ -19,5 +35,5 @@ class ModelPredictor:
                 predicted_class = model.predict(flattened)[0]
                 predictions[label] = predicted_class
             else:
-                predictions[label] = "Mô hình không tồn tại"
+                predictions[label] = "Mô hình không tồn tại hoặc không thể tải"
         return predictions
